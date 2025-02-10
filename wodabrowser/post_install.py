827ebad4e454a6af5130bbc85495040e6054281e
Hannes Nortje <dr.h.nortje@gmail.com>
2025-02-10 10:18:57 +0100

    icon fixes

---
+++ wodabrowser/post_install.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+
+def update_system():
+    """Update system icon cache and desktop database."""
+    try:
+        # Update icon cache
+        subprocess.run(['gtk-update-icon-cache', '/usr/share/icons/hicolor'], check=False)
+        
+        # Update desktop database
+        subprocess.run(['update-desktop-database', '/usr/share/applications'], check=False)
+    except Exception as e:
+        print(f"Warning: Could not update system caches: {e}")
